78696635ce9761d531ef6ed5774bd6647c4b68c3
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2024-05-23 15:29:21 +0100

    Install on php 8.3 and add rector

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/src'])
+    ->withPhpSets(php83: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        //        strictBooleans: true,
+        instanceOf: true,
+        earlyReturn: true
+    )
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        DeclareStrictTypesRector::class
+    ]);
